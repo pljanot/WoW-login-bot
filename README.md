9da051f6dab2755f0d1a15745a6fe4e554b8ec81
pljanot <patricjan@live.com>
2022-10-02 20:30:39 +0200

    Extend readme.md by polish intruction. Adding handling when somone else login to your account. Adding requirments txt for easier installation road

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+# Instrukcja po polsku
+
+1. Zainstaluj python [Python Download exe](https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe)2. 2. podczas instalacji dodaj python'a do sciezki windowsa (checkbox)
+   2. podczas instalacji dodaj python'a do sciezki windowsa (checkbox)
+2. Zainstaluj biblioteki potrzebne do dziaania pythona ! `pip install -r requirments.txt`
+3. Zainstaluj [Instalka win.64](https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.2.0.20220712.exe)
+4. Zmodyfikuj plik `settings` w katalogu glownym (te ponizej wystarcza!)
+   ```
+    login = `twoj login`
+    password = `twoje haslo`
+    acc = `nazwa_twojego_konta_wowa`
+    server = Gehennas
+    WOW_PATH = C:\\World of Warcraft\\_classic_\\WowClassic.exe
+    TES_PATH = C:\\Program Files\\Tesseract-OCR\\tesseract.exe
+   ```
+   
+6. Zaznacz w grze `remember my account / login`
+7. Uruchom linie komend (cmd) i skrypt `run.cmd`
+   6. skrypt uruchomi okienko wow'a i przesunie myszke powoli
+   7. program wykrywa `scene` na ktorej aktualnie sie znajduje (ekran logowania, ekran kolejki, ekran 
+   dissconecta) i reaguje odpowiednio na zadany ekran
+   
+
 # WoW-login-bot
 Bot for automatic login and keeping account online for classic World of Warcraft<br><br>
 <i>WARNING: The bot is done in academical purpose. You use the software as it is and on your own risk. No warranty, guarantee nor sharing of responsibility is supposed in case of use the software</i><br>
@@ -11,7 +34,7 @@ Bot for automatic login and keeping account online for classic World of Warcraft
 Check settings file and update following:<br>
 login - your account name<br>
 password - account password<br>
-acc - combination of down (d) or up (u) arrow keys needed to chose correct account (if there are multiple accounts - just run the bot and you will see what is it). <br><br><i>REMINDER: if check box of "Remember account name" is set, this screen will never appear on startup, however it's possible to see this after disconnect</i><br><br>
+acc - combination of down (d) or up (u) arrow keys needed to chose correct account (only if you have multiple accounts - just run the bot and you will see what is it). <br><br><i>REMINDER: if check box of "Remember account name" is set, this screen will never appear on startup, however it's possible to see this after disconnect</i><br><br>
 server - the server name to login<br>
 WOW_PATH - path to WoW.exe<br>
 TES_PATH - path to tesseract.exe<br>
--- py_service.py
+++ py_service.py
@@ -13,22 +13,20 @@ import pytesseract
 #   Set path from previous step
 
 
-#pip install imutils
+# pip install imutils
 import imutils
 
-#pip install Pillow
+# pip install Pillow
 from PIL import Image, ImageFilter, ImageEnhance
 from pytesseract import image_to_string
 
-#pip install pyautogui
+# pip install pyautogui
 import pyautogui
 
 import operator
 import time
 import random
 from subprocess import Popen
-
-
 
 login = ""
 password = ""
@@ -45,30 +43,32 @@ X_RAND_COEFF = 0.01
 Y_RAND_COEFF = 0.001
 USER = 0
 
-#pytesseract.pytesseract.tesseract_cmd = TES_PATH #r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = TES_PATH #r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 mainScreenWords = ['name', 'password', 'login']
 accountsScreenWords = ['world', 'warcraft', 'accounts', 'wow 1', 'accept']
-realmsScreenWords = ['german', 'realm', 'bloodfang', 'ashbringer', 'dreadmist', 'noggenfogger', 'medium']
+realmsScreenWords = ['english', 'realm', 'gehennas', 'firemaw', 'dreadmist', 'noggenfogger', 'full']
 queueScreenWords = ['change', 'realm', 'queue']
 charScreenWords = ['delete', 'character', 'back', 'create', 'new']
 discScreenWords = ['password', 'login', 'you', 'have', 'been', 'disconnected', 'server']
 noIntScreen = ['you', 'have', 'been', 'disconnected', 'blz51901016']
+loggedInSomewhereElse = ['your', 'account', 'has', 'logged', 'in', 'blz519000060']
 
-scenes = {\
-            0 : mainScreenWords,\
-            1 : accountsScreenWords,\
-            2 : realmsScreenWords,\
-            3 : queueScreenWords,
-            4 : charScreenWords, \
-            5 : discScreenWords, \
-            6 : noIntScreen \
-        }
+scenes = {
+          0: mainScreenWords,
+          1: accountsScreenWords,
+          2: realmsScreenWords,
+          3: queueScreenWords,
+          4: charScreenWords,
+          5: discScreenWords,
+          6: noIntScreen,
+          7: loggedInSomewhereElse
+    }
 
 
 def getSceneId(scenes):
     text = getSceneText().lower()
-    #print(text)
+    print(text)
 
     probabilities = []
     for k in scenes.keys():
@@ -80,128 +80,131 @@ def getSceneId(scenes):
 
     index, value = max(enumerate(probabilities), key=operator.itemgetter(1))
 
-    #print(probabilities)
-    #print(index, value)
+    # print(probabilities)
+    # print(index, value)
     if (value > 0.5):
         return index
     else:
         return -1
 
+
 def getSceneText():
-    #Load img from folder
-    #image = np.array(cv2.imread("5.png"))
+    # Load img from folder
+    # image = np.array(cv2.imread("5.png"))
 
-
-    #Take screenshot
+    # Take screenshot
     image = np.array(pyautogui.screenshot())
     image = image[:, :, ::-1].copy()
 
     width, height = image.shape[:2]
-    cntY, cntX = width//2, height//2
-    minX, maxX, minY, maxY = int(cntX - 0.5*cntX), int(cntX + 0.5*cntX), int(cntY - 0.75*cntY), int(cntY + 0.9*cntY)
+    cntY, cntX = width // 2, height // 2
+    minX, maxX, minY, maxY = int(cntX - 0.5 * cntX), int(cntX + 0.5 * cntX), int(cntY - 0.75 * cntY), int(
+        cntY + 0.9 * cntY)
 
-    #print(width, height)
-    #print(cntX, cntY)
-    #print(minX, maxX, minY, maxY)
+    # print(width, height)
+    # print(cntX, cntY)
+    # print(minX, maxX, minY, maxY)
 
     roi = image[minY:maxY, minX:]
 
-    #cv2.imshow('1',roi)
-    #cv2.waitKey(0)
+    # cv2.imshow('1',roi)
+    # cv2.waitKey(0)
 
     # get yellow text only
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, (15, 190, 190), (36, 255,255))
-    imask = mask>0
+    mask = cv2.inRange(hsv, (15, 190, 190), (36, 255, 255))
+    imask = mask > 0
     res = np.zeros_like(roi, np.uint8)
     res[imask] = roi[imask]
 
-    #cv2.imshow('2',res)
-    #cv2.waitKey(0)
+    # cv2.imshow('2',res)
+    # cv2.waitKey(0)
 
-    #convert to grayscale and enhance
+    # convert to grayscale and enhance
     img = cv2.cvtColor(np.array(res), cv2.COLOR_BGR2GRAY)
     kernel = np.ones((2, 2), np.uint8)
     roi = cv2.dilate(img, kernel, iterations=1)
     roi = cv2.erode(roi, kernel, iterations=1)
 
-    #get text
+    # get text
     sceneText = (pytesseract.image_to_string(roi))
-    #print(sceneText)
+    # print(sceneText)
 
-    #show image
-    #cv2.imshow('3',roi)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    # show image
+    # cv2.imshow('3',roi)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return sceneText
 
+
 def getWhiteSceneText():
-    #Load img from folder
-    #image = np.array(cv2.imread("t2.png"))
+    # Load img from folder
+    # image = np.array(cv2.imread("t2.png"))
 
-
-    #Take screenshot
+    # Take screenshot
     image = np.array(pyautogui.screenshot())
     image = image[:, :, ::-1].copy()
 
     width, height = image.shape[:2]
-    cntY, cntX = width//2, height//2
-    minX, maxX, minY, maxY = int(cntX - 0.5*cntX), int(cntX + 0.5*cntX), int(cntY - 0.75*cntY), int(cntY + 0.9*cntY)
+    cntY, cntX = width // 2, height // 2
+    minX, maxX, minY, maxY = int(cntX - 0.5 * cntX), int(cntX + 0.5 * cntX), int(cntY - 0.75 * cntY), int(
+        cntY + 0.9 * cntY)
 
-    #print(width, height)
-    #print(cntX, cntY)
-    #print(minX, maxX, minY, maxY)
+    # print(width, height)
+    # print(cntX, cntY)
+    # print(minX, maxX, minY, maxY)
 
     roi = image[minY:maxY, minX:cntX]
 
-    #cv2.imshow('1',roi)
-    #cv2.waitKey(0)
+    # cv2.imshow('1',roi)
+    # cv2.waitKey(0)
 
     # get yellow text only
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, (0, 0, 210), (0, 0,255))
-    imask = mask>0
+    mask = cv2.inRange(hsv, (0, 0, 210), (0, 0, 255))
+    imask = mask > 0
     res = np.zeros_like(roi, np.uint8)
     res[imask] = roi[imask]
 
-    #cv2.imshow('2',res)
-    #cv2.waitKey(0)
+    # cv2.imshow('2',res)
+    # cv2.waitKey(0)
 
-
-    #convert to grayscale and enhance
+    # convert to grayscale and enhance
     img = cv2.cvtColor(np.array(res), cv2.COLOR_BGR2GRAY)
     kernel = np.ones((1, 1), np.uint8)
     roi = cv2.dilate(img, kernel, iterations=1)
     roi = cv2.erode(roi, kernel, iterations=1)
 
-    #get text
+    # get text
     sceneText = (pytesseract.image_to_string(roi))
-    #print(sceneText)
+    # print(sceneText)
 
-    #show image
-    #cv2.imshow('3',roi)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    # show image
+    # cv2.imshow('3',roi)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return sceneText
 
 
 def randomWait(a, b):
-    sleepTime = round(random.randint(a*1000,b*1000)/1000, 2)
+    sleepTime = round(random.randint(a * 1000, b * 1000) / 1000, 2)
     print("Sleeping time: " + str(sleepTime))
     time.sleep(sleepTime)
+
 
 def fakeDelay(a=MINDELAY, b=MAXDELAY):
     lwait = a
     hwait = b
-    return round(random.randint(lwait,hwait)/1000, 3)
+    return round(random.randint(lwait, hwait) / 1000, 3)
+
 
 def enterCredentials(login, password):
     print("enter cred")
     if (USER == 1):
-        pyautogui.hotkey('ctrl', 'a', interval=fakeDelay(300,800))
-        pyautogui.press('del', interval=fakeDelay(300,800))
+        pyautogui.hotkey('ctrl', 'a', interval=fakeDelay(300, 800))
+        pyautogui.press('del', interval=fakeDelay(300, 800))
         for l in login:
             if (l.isupper()):
                 pyautogui.press('capslock', interval=fakeDelay())
@@ -212,8 +215,8 @@ def enterCredentials(login, password):
 
         pyautogui.press('tab', interval=fakeDelay())
 
-    pyautogui.hotkey('ctrl', 'a', interval=fakeDelay(300,800))
-    pyautogui.press('del', interval=fakeDelay(300,800))
+    pyautogui.hotkey('ctrl', 'a', interval=fakeDelay(300, 800))
+    pyautogui.press('del', interval=fakeDelay(300, 800))
     for p in password:
         if (p.isupper()):
             pyautogui.press('capslock', interval=fakeDelay())
@@ -222,8 +225,9 @@ def enterCredentials(login, password):
         else:
             pyautogui.press(p, interval=fakeDelay())
 
-    pyautogui.press('enter', interval=fakeDelay(300,800))
+    pyautogui.press('enter', interval=fakeDelay(300, 800))
     print("exit cred")
+
 
 def chooseAccount(acc):
     for k in acc:
@@ -231,37 +235,35 @@ def chooseAccount(acc):
             pyautogui.press('down', interval=fakeDelay())
         if (k == 'u'):
             pyautogui.press('up', interval=fakeDelay())
-    pyautogui.press('enter', interval=fakeDelay(300,800))
+    pyautogui.press('enter', interval=fakeDelay(300, 800))
+
 
 def chooseServer(server, servers):
-
     print("start choose")
     windowSize = pyautogui.size()
     xStep = int(windowSize[0] * X_STEP_COEFF)
-    yStep =  int(windowSize[1] * Y_STEP_COEFF)
+    yStep = int(windowSize[1] * Y_STEP_COEFF)
     xRand = int(windowSize[0] * X_RAND_COEFF)
     yRand = int(windowSize[1] * Y_RAND_COEFF)
-    x = windowSize[0]//2 + random.randint(-xRand,xRand)
-    y = 2*windowSize[1]//6 + random.randint(-yRand,yRand)
-    #print(x,y)
-    pyautogui.moveTo(x, y, duration = fakeDelay(600,1200))
-    randomWait(0.5,0.7)
+    x = windowSize[0] // 2 + random.randint(-xRand, xRand)
+    y = 2 * windowSize[1] // 6 + random.randint(-yRand, yRand)
+    # print(x,y)
+    pyautogui.moveTo(x, y, duration=fakeDelay(600, 1200))
+    randomWait(0.5, 0.7)
     pyautogui.click(x, y)
-    randomWait(0.7,0.9)
-
+    randomWait(0.7, 0.9)
 
     whiteText = getWhiteSceneText().lower()
     last = whiteText
     dir = -1
     while (not server in whiteText):
-        x = x + random.randint(-xStep,xStep)
-        y = y + dir*yStep + dir*random.randint(0,yRand)
-        pyautogui.moveTo(x, y, duration = fakeDelay(400,800))
+        x = x + random.randint(-xStep, xStep)
+        y = y + dir * yStep + dir * random.randint(0, yRand)
+        pyautogui.moveTo(x, y, duration=fakeDelay(400, 800))
         pyautogui.click(x, y)
         time.sleep(0.5)
         whiteText = getWhiteSceneText().lower()
         canGo = True
-
 
         for s in servers.keys():
             if (not canGo): break
@@ -275,18 +277,19 @@ def chooseServer(server, servers):
 
         if (last == whiteText):
             dir = dir * (-1)
-            x = windowSize[0]//2 + random.randint(-xRand,xRand)
-            y = windowSize[1]//2 + random.randint(-yRand,yRand)
+            x = windowSize[0] // 2 + random.randint(-xRand, xRand)
+            y = windowSize[1] // 2 + random.randint(-yRand, yRand)
 
         last = whiteText
         print(last, dir)
 
-    pyautogui.press('enter', interval=fakeDelay(300,800))
+    pyautogui.press('enter', interval=fakeDelay(300, 800))
     print("exit choose")
+
 
 def updateSettings():
     print("enter settings")
-    fS = open("settings","r")
+    fS = open("settings", "r")
     fS = fS.readlines()
     global login, password, acc, server, WOW_PATH, \
         TES_PATH, MINDELAY, MAXDELAY, X_STEP_COEFF, \
@@ -309,8 +312,9 @@ def updateSettings():
     pytesseract.pytesseract.tesseract_cmd = TES_PATH
     print("exit settings")
 
+
 def idler():
-    fS = open("settings","r")
+    fS = open("settings", "r")
     fS = fS.readlines()
     servers = {}
     for l in fS[13:]:
@@ -320,65 +324,68 @@ def idler():
             tmpList.append(w.lower())
         servers[l.rstrip().lstrip()] = tmpList
 
-    #print(servers)
+    # print(servers)
 
     updateSettings()
 
     Popen([WOW_PATH])
-    pyautogui.moveTo(int(pyautogui.size()[0]/2) + int(pyautogui.size()[0]/6), int(pyautogui.size()[1]/2), duration = fakeDelay(2000,4000))
+    pyautogui.moveTo(int(pyautogui.size()[0] / 2) + int(pyautogui.size()[0] / 6), int(pyautogui.size()[1] / 2),
+                     duration=fakeDelay(2000, 4000))
     last = -1
     unknownCnt = 0
-    while(True):
+    while (True):
         state = getSceneId(scenes)
-        print(state)
+        print(f"state: {state}")
+        print(f"last: {last}")
 
-        if (state <= last and not (state == 3 or state == 4 or state == 5 or state == 6)): state = -1
+        if state <= last and not (state == 3 or state == 4 or state == 5 or state == 6):
+            state = -1
         else:
             last = state
             unknownCnt = 0
 
-        if (state == 0):
+        if state == 0:
             enterCredentials(login, password)
-            randomWait(0.5,1)
-        elif (state == 1):
+            randomWait(0.5, 1)
+        elif state == 1:
             chooseAccount(acc)
             randomWait(0.5, 1)
-        elif (state == 2):
+        elif state == 2:
             chooseServer(server, servers)
-            randomWait(1,3)
-        elif (state == 3 or state == 4):
-            if (state == 4):
-                k = random.randint(0,10)
-                if (k == 1):
+            randomWait(1, 3)
+        elif state == 3 or state == 4:
+            if state == 4:
+                k = random.randint(0, 10)
+                if k == 1:
                     pyautogui.press('down', interval=fakeDelay())
-                if (k == 3):
+                if k == 3:
                     pyautogui.press('up', interval=fakeDelay())
             randomWait(5, 10)
-        elif (state == 5):
-            randomWait(0.5,1)
+        elif state == 5 or state == 7:
+            randomWait(0.5, 1)
             pyautogui.press('enter')
-            randomWait(1,3)
+            randomWait(1, 3)
             last = -1
-        elif (state == 6):
+        elif state == 6:
             guiSize = pyautogui.size()
-            x_cnt, y_cnt = int(11*guiSize[0]//20), int(515*guiSize[1]//1000)
-            #print(guiSize, x_cnt, y_cnt)
-            pyautogui.moveTo(x_cnt, y_cnt, duration = fakeDelay(1000, 3000))
+            x_cnt, y_cnt = int(11 * guiSize[0] // 20), int(515 * guiSize[1] // 1000)
+            # print(guiSize, x_cnt, y_cnt)
+            pyautogui.moveTo(x_cnt, y_cnt, duration=fakeDelay(1000, 3000))
             randomWait(0.5, 1)
             pyautogui.click(x_cnt, y_cnt)
             print("CLICKED ON BUTTON!")
             randomWait(0.5, 1)
-            if (USER == 1):
+            if USER == 1:
                 pyautogui.press('tab')
             last = -1
         else:
             print("WAITING STATE!")
             unknownCnt += 1
-            if (unknownCnt >= 10):
+            if unknownCnt >= 10:
                 guiSize = pyautogui.size()
-                x_cnt, y_cnt = int(guiSize[0]//2), int(guiSize[1]//2)
+                x_cnt, y_cnt = int(guiSize[0] // 2), int(guiSize[1] // 2)
                 print(guiSize, x_cnt, y_cnt)
-                pyautogui.moveTo(x_cnt, y_cnt, duration = fakeDelay(1000, 3000))
+                pyautogui.moveTo(x_cnt, y_cnt, duration=fakeDelay(1000, 3000))
                 randomWait(0.5, 1)
                 pyautogui.click(x_cnt, y_cnt)
                 print("CLICKED ON CNT!")
@@ -386,9 +393,10 @@ def idler():
                 pyautogui.press('enter')
                 unknownCnt = 0
                 randomWait(0.5, 1)
-                pyautogui.moveTo(int(pyautogui.size()[0]/2) + int(pyautogui.size()[0]/6), int(pyautogui.size()[1]/2), duration = fakeDelay(2000,4000))
+                pyautogui.moveTo(int(pyautogui.size()[0] / 2) + int(pyautogui.size()[0] / 6),
+                                 int(pyautogui.size()[1] / 2), duration=fakeDelay(2000, 4000))
                 randomWait(0.5, 1)
-            randomWait(1,3)
+            randomWait(1, 3)
 
 
 idler()
